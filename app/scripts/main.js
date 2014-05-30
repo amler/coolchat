675ddf861b729f321a510c09a69dfa5fcf98585d
Heather Amler <heatheramler@yahoo.com>
2014-05-30 12:12:36 -0400

    Initial Commit

---
+++ app/scripts/main.js
@@ -0,0 +1,1 @@
+//$.getJSON('http://tiny-pizza-server.herokuapp.com/collections/chat-messages').done(setUserData);
---
+++ test/spec/test.js
@@ -0,0 +1,44 @@
+/* global describe, it */
+
+(function () {
+    'use strict';
+
+    describe('Chat Constructor', function () {
+        describe('constructs the object to be sent to the test server', function () {
+            it('should not accept an empty sting as a user name', function () {
+            	var userName = function () {
+	        		slideshow('string');
+	        	}
+            	expect
+            });
+
+  	      	it('should not accept more than 15 characters for a user name.', function(){
+  	        	var stringArray = function () {
+  	        		slideshow('string');
+  	        	}
+  	        	expect(stringArray).to.throw(Error)
+  	      	});
+
+            it('should not accept numbers for a user name.', function () {
+              var answer = 2;
+              expect(2).to.equal(answer);
+              player3.should.have.property('diceSides', 5)
+            });
+
+            it('should run here few assertions', function () {
+              var answer = 2;
+              expect(2).to.equal(answer);
+            });
+
+            it('should run here few assertions', function () {
+              var answer = 2;
+              expect(2).to.equal(answer);
+            });
+
+            it('should run here few assertions', function () {
+              var answer = 2;
+              expect(2).to.equal(answer);
+            });
+        });
+    });
+})();
