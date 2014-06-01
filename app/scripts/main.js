62d9e7b7dc87510a93095c3a94b6b12f4c5ecc97
Heather Amler <heatheramler@yahoo.com>
2014-06-01 13:00:29 -0400

    Date attempt

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,4 +1,3 @@
-//$.getJSON(serverUrl).done(setUserData);
 
 //////////////////////////////////////////////////
 // Variables
@@ -6,6 +5,7 @@
 
 var serverUrl = 'http://tiny-pizza-server.herokuapp.com/collections/chat-messages/';
 var chat = null;
+var current = 0;
 
 //////////////////////////////////////////////////
 // Chat Object
@@ -22,8 +22,8 @@ Chat.prototype.sendMessage = function(message) {
 		user: this.username,
 		message: message,
 		time: Date.now(),
-		meta: "",
-		appID: "Nunya"
+		meta: '',
+		appID: 'secret'
 	};
 
 	// submit to server
@@ -45,13 +45,112 @@ function displayGreeting (userName) {
 	$('#login').hide();
 	$('#message-input').show();
 }
-
 displayLogin();
+
+//////////////////////////////////////////////////
+// Format Time Function
+//////////////////////////////////////////////////
+
+function timeSince(date) {
+    
+    date = new Date(date);
+    // how many seconds ago / by thousand to get seconds
+    var seconds = Math.floor((new Date() - date) / 1000);
+    //   how many seconds ago divided by seconds in a year
+    var interval = Math.floor(seconds / 31536000);
+    
+    var months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'June', 'July', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec'];
+
+    var weekdays = [ 'Sun','Mon', 'Tue', 'Wed', 'Thu','Fri', 'Sat'];
+
+
+    if (interval > 1) {
+        // return Nov 25, 2013 --- 
+        var year = date.getFullYear();
+        return months[date.getMonth()] + ' ' + date.getDate() + ', ' + year;
+    }
+
+    // > 7 days
+    interval = Math.floor(seconds / 604800);
+    if (interval > 1) {
+        // return Apr 9
+        return months[date.getMonth()] + ' ' + date.getDate();
+    }
+
+    // > day
+    interval = Math.floor(seconds / 86400);
+    if (interval > 1) {
+      // return Thursday at 1:20am
+      var day = date.getDay();
+      var weekDay = (weekdays[day]);
+      var hours = date.getHours();
+      var minutes = date.getMinutes();
+      if (hours < 12) {
+        // return Thursday at 10:00am
+        return weekDay + ' at ' + hours + ':' + minutes + 'am';
+     
+      } else if (hours > 12){
+        //return Thursday at 1:00pm
+        hours = hours - 12;
+        return weekDay + ' at ' + hours + ':' + minutes + 'pm';
+
+      } else if (hours == 12) {
+        // return Thursday 12:00pm
+        return weekDay + ' at ' + hours + ':' + minutes + 'pm';
+      }
+      return interval + ' days';
+    }
+
+    // > hour
+    interval = Math.floor(seconds / 3600);
+    if (interval > 1) {
+        return interval + ' hours';
+    }
+    interval = Math.floor(seconds / 60);
+    
+    // > minute
+    if (interval > 1) {
+        return interval + ' minutes';
+    }
+
+    return Math.floor(seconds) + ' seconds';
+}
+
+
+//////////////////////////////////////////////////
+// Render Function
+//////////////////////////////////////////////////
+
+function renderData (usersData) {
+
+	var chatTemplate = $('#all-messages').html();
+	var templateMethod = _.template(chatTemplate);
+	usersData.reverse();
+
+	usersData.forEach(function (message){
+		message.time = timeSince(message.time);
+	// putting each repo in my template to be rendered 
+	var rendered = templateMethod(message);
+
+	// actually rendering the content to div with the class repocontent.  
+	$('.chat-content').append(rendered);
+
+	// track last id 
+
+	// intervally check new chats	
+
+  });
+}
+
+function newChats (lastid) {
+	// compare last id with most recent
+
+	// if new id render??
+}
 
 //////////////////////////////////////////////////
 // Click Events
 //////////////////////////////////////////////////
-
 
 $('#login button').click(function(){
 	// get the username value
@@ -64,7 +163,7 @@ $('#login button').click(function(){
 	// executes a search for a match between a regex filter and a specified string
 	if (/[^a-zA-Z0-9]/.test(userName)) {
 
-		alert("Dude, try using some letters or numbers.");
+		alert('Dude, try using some letters or numbers.');
 		throw new Error ('Invalid user name characters.');
 		return;
 
@@ -108,5 +207,5 @@ $('.message-button').click(function(){
 	$('.chat-message').val('');
 });
 
-//$.getJSON(serverUrl).done(renderData);
+$.getJSON(serverUrl).done(renderData);
 
